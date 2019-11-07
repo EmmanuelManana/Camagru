9225beff79795f21e3c2d316ac271af23398774a
EmmanuelManana <emanana@student.wethinkcode.co.za>
2019-11-07 22:06:49 +0200

    mail fix

--- mail.php
+++ mail.php
@@ -1,4 +1,11 @@
 <?php
 
-    mail("mananaemmanuel3@gmail.com", "test", "clickhere");
+   if ( mail("mananaemmanuel3@gmail.com", "test", "click here"))
+   {
+       echo "echo success email has been sent";
+   }
+   else 
+   {
+       echo " sendind email failed";
+   }
 ?>
