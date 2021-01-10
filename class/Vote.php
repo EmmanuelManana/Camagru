be29c4884f00109514b99388d13121f81fc8ff64
martian1431 <pmalope@student.wethinkcode.co.za>
2021-01-10 15:03:41 +0200

    like notification temporialy working

--- class/Vote.php
+++ class/Vote.php
@@ -8,10 +8,22 @@ class Vote {
 
 	private function addLike($db, $up_id, $us_id, $val)
 	{
-		$db->query("INSERT INTO `likes` SET `tab` = 'img', `up_id` = ?, `us_id` = ?, `like_val` = ?", [$up_id, $us_id, $val,]);
+        echo "User ID: " . $us_id . " Val: " . $val . "up_id: " . $up_id;
+        $db->query("INSERT INTO `likes` SET `tab` = 'img', `up_id` = ?, `us_id` = ?, `like_val` = ?", [$up_id, $us_id, $val,]);
 		if ($val > 0)
 		{
-		$db->query("UPDATE `img` SET `up_likes` = `up_likes` + ? WHERE `up_id` = ?", [$val, $up_id, ]);
+		    $db->query("UPDATE `img` SET `up_likes` = `up_likes` + ? WHERE `up_id` = ?", [$val, $up_id, ]);
+            $imageAuthor = $db->query("SELECT img.up_id, user.login, user.email, user.mail_com FROM img INNER JOIN user ON img.up_usid = user.id")->fetch();
+            if ((int)$imageAuthor->mail_com == 1){
+                $liker = $_SESSION['auth']->login;
+                $to = $imageAuthor->email;
+                $subject = 'Like';
+                $message = " Hello !\n\n$liker liked one of your Camagru photos! \n\n\"\"";
+                $headers = 'From: emanana@student.wethinkcode.co.za' . "\r\n" .
+                    'Reply-To: emanana@student.wethinkcode.co.za' . "\r\n" .
+                    'G_MAIL: PHP/' . phpversion();
+                mail($to, $subject, $message, $headers);
+            }
 		}
 		else
 		{
@@ -38,9 +50,9 @@ class Vote {
 			$up_id,
 			]);
 		if ($req->rowCount() > 0){
-			$like_check = self::likeCheck($db, $us_id, $up_id);
+            $like_check = self::likeCheck($db, $us_id, $up_id);
 			if ($like_check == false){
-				self::addLike($db, $up_id, $us_id, $val);
+                self::addLike($db, $up_id, $us_id, $val);
 				return true;
 			}else if ($like_check->like_val != $val){
 				self::cancelLike($db, $up_id, $us_id, $val);
